886a391ea3f7f372db1aed136e6389a3a6a5431c
Luis Feliz <Luisfeliz@gmail.com>
2024-02-15 09:47:57 -0500

    Setting Category functionality

--- src/Components/CarouselCategory.jsx
+++ src/Components/CarouselCategory.jsx
@@ -1,10 +1,26 @@
 import {Swiper, SwiperSlide} from 'swiper/react'
 import {Navigation} from 'swiper/modules'; //this is a librabry for carousel to add arrows for navigation
-
+import { useNavigate, createSearchParams } from 'react-router-dom'
 import 'swiper/css'
 import 'swiper/css/navigation'
 
 const CarouselCategory = () => {
+
+    const navigate = useNavigate();
+
+    const searchCategory = (category) => { 
+        
+        navigate({
+            pathname : "search",
+            search : `${
+              createSearchParams({
+                category: `${category}`,
+                searchTerm : ``
+              })
+            }`
+    
+           })
+     };
   return (
     <div className='bg-white m-3 '>
 
@@ -19,36 +35,36 @@ const CarouselCategory = () => {
 
         
         >
-            <SwiperSlide>
+            <SwiperSlide onClick={()=>  searchCategory("Deals")} className='cursor-pointer'>
                 <img
                 src={"../images/category_0.jpg"}
                 />
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide onClick={()=>  searchCategory("Amazon")} className='cursor-pointer'>
                 <img
                 src={"../images/category_1.jpg"}
                 />
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide onClick={()=>  searchCategory("Fashion")} className='cursor-pointer'>
                 <img
                 src={"../images/category_2.jpg"}
                 />
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide onClick={()=>  searchCategory("Computers")} className='cursor-pointer'>
                 <img
                 src={"../images/category_3.jpg"}
                 />
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide  onClick={()=>  searchCategory("Home")} className='cursor-pointer'>
                 <img
                 src={"../images/category_4.jpg"}
                 />
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide onClick={()=>  searchCategory("Mobiles")} className='cursor-pointer'>
                 <img
                 src={"../images/category_5.jpg"}
                 />
-            </SwiperSlide>
+            </SwiperSlide >
 
         </Swiper>
     </div>
